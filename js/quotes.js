1d7fe1b24b0fffc9a884bdf6c0d425080dcfc52e
Carlos Braga <cbxonline10@gmail.com>
2017-07-21 10:51:25 +1000

    Placing quotes in the quotes.js file

---
+++ js/quotes.js
@@ -0,0 +1,32 @@
+// Array contains 6 items
+var quotes = [{
+    quote: 'You can do anything but not everything',
+    source: 'David Allen',
+    citation: 'Making It All Work',
+    year: 2009,
+    tags: 'Life'
+}, {
+    quote: 'Every man is like a seed planted by the Universe waiting to become a big and flowery tree to join its immense forest',
+    source: 'Carlos Braga',
+    citation: 'instagram.com/kze_',
+    tags: 'Inspirational'
+}, {
+    quote: 'It takes 20 years to build a reputation and five minutes to ruin it. If you think about that, you\'ll do things differently',
+    source: 'Warren Buffet',
+    tags: 'Reputation'
+}, {
+    quote: 'Design is not just what it looks like and feels like. Design is how it works',
+    source: 'Steve Jobs',
+    citation: 'New York Times',
+    year: 2003,
+    tags: 'Design'
+}, {
+    quote: 'The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart',
+    source: 'Helen Keller',
+    year: 1903,
+    tags: 'Inspirational'
+}, {
+    quote: 'The future belongs to those who believe in the beauty of their dreams',
+    source: 'Eleanor Roosevelt',
+    tags: 'Inspirational'
+}];
